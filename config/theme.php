f3cce1ef310718aae295fd715567de306e5ad6f6
jabber <2898117012@qq.com>
2017-12-27 15:54:42 +0800

    refactor: add config template file

---
+++ config/theme.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'active' => 'default',
+    'paths'  => [
+        'absolute' => public_path('themes'),
+        'base'     => 'themes',
+        'assets'   => 'assets'
+    ]
+];
