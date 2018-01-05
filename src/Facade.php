3f39564eb403667b85a495a4af6b2a85050d151c
jabber <2898117012@qq.com>
2018-01-05 17:44:38 +0800

    refactor: add facade and service provider

--- src/Facade.php
+++ src/Facade.php
@@ -2,8 +2,11 @@
 
 namespace LaravelTheme;
 
+use Illuminate\Support\Facades\Facade as LaravelFacade;
+use Themes\Theme;
 
-class Facade
-{
-
+class Facade extends LaravelFacade {
+    protected static function getFacadeAccessor() {
+        return Theme::class;
+    }
 }
