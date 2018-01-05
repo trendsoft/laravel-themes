73618a3d5ca55d66be18db7f099d67e39dd95ce2
jabber <2898117012@qq.com>
2018-01-05 17:44:09 +0800

    test: add test case

--- tests/Unit/FacadeTest.php
+++ tests/Unit/FacadeTest.php
@@ -5,7 +5,8 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 
-class FacadeTest extends TestCase
-{
-    
+class FacadeTest extends TestCase {
+    public function testFacadeIsAvailable() {
+        $this->assertTrue($this->app->isAlias('Theme'));
+    }
 }
--- tests/Unit/ServiceProviderTest.php
+++ tests/Unit/ServiceProviderTest.php
@@ -4,8 +4,12 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Themes\Theme;
 
-class ServiceProviderTest extends TestCase
-{
-    
+class ServiceProviderTest extends TestCase {
+
+    public function testServiceProviderIsAvailable() {
+        $this->assertTrue($this->app->bound(Theme::class));
+        $this->assertTrue($this->app->make(Theme::class));
+    }
 }
